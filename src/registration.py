ac46fc022db1d0e3f32681addfea0bb40c8ab8ee
naziaaamer <nazia.aamer89@gmail.com>
2026-09-18 13:43:03 +0200

    push the changes to test the failing yml workflow

---
+++ src/registration.py
@@ -0,0 +1,6 @@
+class Registration:
+    def __init__(self):
+        self.paid = False
+
+    def fee_is_paid(self):
+        pass
---
+++ tests/unit/test_registration.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.registration import Registration
+
+@pytest.mark.unit
+def test_registration_fee_is_paid():
+    #Arrange
+    registration = Registration()
+
+    #Act
+    result = registration.fee_is_paid()
+
+    #Assert
+    assert result is True
